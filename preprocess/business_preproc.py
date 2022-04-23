9114d43d5368e9fb0a0b01aca59f4f978a85f18b
mvijay97 <malaika.vijay@gmail.com>
2022-04-23 14:57:12 -0400

    Add cleaned business csv

---
+++ preprocess/business_preproc.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--in-file", type=str)
+parser.add_argument("--out-file", type=str)
+
+keep_attrs = ['business_id', 'name', 'address', 'city', 'state', 'postal_code',
+              'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'categories', 'attributes.WiFi']
+
+bool_attrs = [
+    'attributes.ByAppointmentOnly',
+    'attributes.BusinessAcceptsCreditCards',
+    'attributes.BikeParking',
+    'attributes.CoatCheck',
+    'attributes.RestaurantsTakeOut',
+    'attributes.RestaurantsDelivery',
+    'attributes.Caters',
+    'attributes.WheelchairAccessible',
+    'attributes.HappyHour',
+    'attributes.OutdoorSeating',
+    'attributes.HasTV',
+    'attributes.RestaurantsReservations',
+    'attributes.DogsAllowed',
+    'attributes.GoodForKids',
+    'attributes.BusinessAcceptsBitcoin',
+    'attributes.Smoking',
+]
+
+
+def load_json(path):
+    f = open(path)
+    data = [json.loads(line) for line in f]
+    raw_business_df = pd.json_normalize(data, max_level=5)
+    return raw_business_df
+
+
+def clean_wifi(df):
+    wifi_categories = {"u'free'": "'free'", "u'no'": "'no'", "u'paid'": "'paid'"}
+    df['attributes.WiFi'] = df['attributes.WiFi'].replace(wifi_categories)
+    df['attributes.WiFi'] = df['attributes.WiFi'].fillna("'no'")
+    return df
+
+
+def clean_categories(df):
+    df['categories'] = df['categories'].apply(lambda x: str(x).split(','))
+    return df
+
+
+def clean_name(df):
+    df['name'] = df['name'].apply(lambda x: x.replace('"', ''))
+    return df
+
+
+def filter(df):
+    df = df[df['is_open'] == True]
+    df = df[df.categories.notna()]
+    df = df[df['categories'].str.contains("Restaurant|Restaurants|Food") == True]
+    return df
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    df = load_json(args.in_file)
+    df = filter(df)
+    df = clean_name(df)
+    df = clean_categories(df)
+    df = clean_wifi(df)
+
+    df[bool_attrs] = df[bool_attrs].fillna(False)
+
+    all_attrs = keep_attrs + bool_attrs
+    df = df[all_attrs]
+
+    df.to_csv(args.out_file, header=True)
