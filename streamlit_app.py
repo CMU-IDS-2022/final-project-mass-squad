ab01530fd5b041aff688ce7b5610653d87c1aa7f
mvijay97 <malaika.vijay@gmail.com>
2022-04-26 00:44:22 -0400

    add style changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,133 +85,158 @@ def welcome_page():
 
 
 def specific_restaurant():
-    print(merged_df)
-    all_business_ids = merged_df['name'].unique().tolist()
+    st.markdown('<style>' + open('icons.css').read() + '</style>', unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: black;'Deep Dive into Your Business!</h2>",
+                unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([1, 5, 1])
+    with col2:
+        st.markdown(
+            "<p> <span class='material-icons'> monitor_heart </span> You're here because you need to check the pulse "
+            "of your business. Interested in what your customers have to say about your restaurant? Are they mad that "
+            "you don't have free WiFi? Do they think your bread is stale? Do they love your new outdoor seating? </p>",
+            unsafe_allow_html=True)
+        st.markdown(
+            "<p> <span class='material-icons'> thumbs_up_down </span> Use the visualizations on this page to study "
+            "the kinds of reviews your restaurant attracts. You can "
+            "also analyze the distribution of your ratings and how your ratings have changed over time </p>",
+            unsafe_allow_html=True)
 
-    '''
-    ## Analyse your business with visualizations!
-    '''
-    feature_selectbox = st.selectbox("Select the name of your business", all_business_ids)
+        st.markdown(
+            "<p> <<span class='material-icons'> rate_review </span> Use the wordcloud to understand what words are "
+            "frequently used to describe experiences at your restaurant. Use the histogram and the line graph to "
+            "study the distribution of ratings over time! Hover over these plots to see a sample of reviews!</p>",
+            unsafe_allow_html=True)
 
-    my_business_df = merged_df[merged_df['name'] == feature_selectbox]
+    col1, col2, col3 = st.columns([1, 9, 1])
+    with col2:
 
-    sentiment_range = st.slider('Select the sentiment range you wish to explore', -1.0, 1.0, (-0.5, 0.5))
-    # wordcloud based on sentiment
-    net_df = my_business_df[my_business_df['polarity'] > sentiment_range[0]]
-    net_df = net_df[net_df['polarity'] < sentiment_range[1]]
-    # selected reviews
-    st.dataframe(net_df)
-    
-    if not net_df.empty:
-    ## WORDCLOUD FOR BOTH POSITIVE AND NEGATIVE REVIEWS
-        stopword_set = set(stopwords.words('english') + list(ENGLISH_STOP_WORDS))
-        full_text = ' '.join(net_df['text'].dropna())
-        # negative sentiment
-        if sentiment_range[1] < 0.5:
-            cloud_no_stopword = WordCloud(background_color='white', stopwords=stopword_set,colormap='Reds', width=800, height=400, repeat=True).generate(full_text)
-        # positive sentiment
+
+        all_business_ids = merged_df['name'].unique().tolist()
+
+        '''
+        ## Analyse your business with visualizations!
+        '''
+        feature_selectbox = st.selectbox("Select the name of your business", all_business_ids)
+
+        my_business_df = merged_df[merged_df['name'] == feature_selectbox]
+
+        sentiment_range = st.slider('Select the sentiment range you wish to explore', -1.0, 1.0, (-0.5, 0.5))
+        # wordcloud based on sentiment
+        net_df = my_business_df[my_business_df['polarity'] > sentiment_range[0]]
+        net_df = net_df[net_df['polarity'] < sentiment_range[1]]
+        # selected reviews
+        # st.dataframe(net_df)
+
+        if not net_df.empty:
+        ## WORDCLOUD FOR BOTH POSITIVE AND NEGATIVE REVIEWS
+            stopword_set = set(stopwords.words('english') + list(ENGLISH_STOP_WORDS))
+            full_text = ' '.join(net_df['text'].dropna())
+            # negative sentiment
+            if sentiment_range[1] < 0.5:
+                cloud_no_stopword = WordCloud(background_color='white', stopwords=stopword_set,colormap='Reds', width=800, height=400, repeat=True).generate(full_text)
+            # positive sentiment
+            else:
+                cloud_no_stopword = WordCloud(background_color='white', stopwords=stopword_set,colormap='Greens', width=800, height=400, repeat=True).generate(full_text)
+            plt.imshow(cloud_no_stopword, interpolation='bilinear')
+            plt.axis('off')
+            plt.show()
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot()
         else:
-            cloud_no_stopword = WordCloud(background_color='white', stopwords=stopword_set,colormap='Greens', width=800, height=400, repeat=True).generate(full_text)
-        plt.imshow(cloud_no_stopword, interpolation='bilinear')
-        plt.axis('off')
-        plt.show()
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        #st.pyplot()
-    else:
-        st.write('No reviews available for one of the word cloud columns please pick one which has reviews')
+            st.write('No reviews available for one of the word cloud columns please pick one which has reviews')
 
 
-    '''
-    ## STAR DISTRIBUTION
-    '''    
-    if st.checkbox("Show Star Rating Count"):
-        df = my_business_df.rename(columns={'stars_review':'Count'})['Count'].value_counts()
-        st.dataframe(df)
-    distribution_df = my_business_df['stars_review'].value_counts().reset_index()
-    
-    distribution_df.rename(columns = {'stars_review':'Count', 'index':'Rating'}, inplace = True)
-    
-    reviews_cols_star = {'useful': {}}
-    all_ratings = distribution_df['Rating'].tolist()
-    for rating in all_ratings:
-        rating_df = my_business_df[my_business_df["stars_review"] == rating]
-        useful_review = rating_df.reset_index().drop_duplicates(subset=["stars_review"])
-        reviews_cols_star['useful'][rating] = useful_review['text'].item()
-        
+        '''
+        ## STAR DISTRIBUTION
+        '''
+        if st.checkbox("Show Star Rating Count"):
+            df = my_business_df.rename(columns={'stars_review':'Count'})['Count'].value_counts()
+            st.dataframe(df)
+        distribution_df = my_business_df['stars_review'].value_counts().reset_index()
 
-    star_useful = pd.DataFrame.from_dict(reviews_cols_star['useful'].items())
-    
-    star_useful.columns = ['Rating', 'Voted most useful review']
-    
-    star_useful.set_index('Rating', inplace=True)
-    distribution_df.set_index('Rating', inplace=True)
-    
-    distribution_df = distribution_df.join(star_useful.astype(str))
-    distribution_df.reset_index(inplace=True)
-    
-    distribution_chart = alt.Chart(distribution_df).mark_bar().encode(
-    y=alt.Y('Count', title="Rating Count"),
-    x=alt.X('Rating:N', title = "Star-rating"),
-    tooltip=["Count","Voted most useful review"]
-    ).properties(
-        title="Star Rating Distribution",
-        width=800,height=600
+        distribution_df.rename(columns = {'stars_review':'Count', 'index':'Rating'}, inplace = True)
 
-    ).interactive()
-    st.altair_chart(distribution_chart)
+        reviews_cols_star = {'useful': {}}
+        all_ratings = distribution_df['Rating'].tolist()
+        for rating in all_ratings:
+            rating_df = my_business_df[my_business_df["stars_review"] == rating]
+            useful_review = rating_df.reset_index().drop_duplicates(subset=["stars_review"])
+            reviews_cols_star['useful'][rating] = useful_review['text'].item()
 
-    '''
-    ## RATING OVER TIME
-    '''
-    my_business_df['date'] = pd.to_datetime(my_business_df['date'])
-    date_df = my_business_df.groupby(my_business_df.date.dt.year).mean()
-    if st.checkbox("Show Year-wise Average Star Rating"):
-        st.dataframe(date_df['stars_review'])
-    
-    # get best and worst review for each year    
-    all_years = date_df.index.values.tolist()#
-    reviews_cols = {'useful': {}, 'best': {}, 'worst': {}}
-    for year in all_years:
-        year_df = my_business_df[my_business_df["date"].dt.year == year]
-        useful_count = year_df['useful'].max()
-        best_count = year_df['stars_review'].max()
-        worst_count = year_df['stars_review'].min()
-        useful_review = my_business_df[my_business_df['useful'] == useful_count].reset_index().drop_duplicates(subset=["useful"])
-        useful_review['date'] = year
-        best_review = my_business_df[my_business_df['stars_review'] == best_count].reset_index().drop_duplicates(subset=["stars_review"])
-        worst_review = my_business_df[my_business_df['stars_review'] == worst_count].reset_index().drop_duplicates(subset=["stars_review"])
-        reviews_cols['useful'][year] = useful_review['text'].item()
-        reviews_cols['best'][year] = best_review['text'].item()
-        reviews_cols['worst'][year] = worst_review['text'].item()
 
-    most_useful_df = pd.DataFrame.from_dict(reviews_cols['useful'].items())
-    most_useful_df.columns = ['Year', 'Voted most useful review']
-    most_useful_df.set_index('Year', inplace=True)
-    date_df = date_df.join(most_useful_df.astype(str))
+        star_useful = pd.DataFrame.from_dict(reviews_cols_star['useful'].items())
 
-    best_df = pd.DataFrame.from_dict(reviews_cols['best'].items())
-    best_df.columns = ['Year', 'Most Positive Review']
-    best_df.set_index('Year', inplace=True)
-    date_df = date_df.join(best_df.astype(str))
+        star_useful.columns = ['Rating', 'Voted most useful review']
 
-    worst_df = pd.DataFrame.from_dict(reviews_cols['worst'].items())
-    worst_df.columns = ['Year', 'Most Negative Review']
-    worst_df.set_index('Year', inplace=True)
-    date_df = date_df.join(worst_df.astype(str))
+        star_useful.set_index('Rating', inplace=True)
+        distribution_df.set_index('Rating', inplace=True)
 
-    date_df = date_df.reset_index()
+        distribution_df = distribution_df.join(star_useful.astype(str))
+        distribution_df.reset_index(inplace=True)
 
-    ratings_chart = alt.Chart(date_df).encode(
-        x=alt.X('date:T', title="Year"),
-        y=alt.Y('stars_review:Q', title = "Average Star rating from Reviews"),
-        tooltip = ['stars_review', 'Voted most useful review', 'Most Positive Review', 'Most Negative Review']
-    ).properties(
-        title="Average Star Ratings over Time",
-        width=800, 
-        height=400
-    )
-    st.altair_chart((ratings_chart.mark_line() + ratings_chart.mark_point(filled=True, size=40)).interactive())
-    return feature_selectbox
+        distribution_chart = alt.Chart(distribution_df).mark_bar().encode(
+        y=alt.Y('Count', title="Rating Count"),
+        x=alt.X('Rating:N', title = "Star-rating"),
+        tooltip=["Count","Voted most useful review"]
+        ).properties(
+            title="Star Rating Distribution",
+            width=800,height=600
+
+        ).interactive()
+        st.altair_chart(distribution_chart)
+
+        '''
+        ## RATING OVER TIME
+        '''
+        my_business_df['date'] = pd.to_datetime(my_business_df['date'])
+        date_df = my_business_df.groupby(my_business_df.date.dt.year).mean()
+        if st.checkbox("Show Year-wise Average Star Rating"):
+            st.dataframe(date_df['stars_review'])
+
+        # get best and worst review for each year
+        all_years = date_df.index.values.tolist()#
+        reviews_cols = {'useful': {}, 'best': {}, 'worst': {}}
+        for year in all_years:
+            year_df = my_business_df[my_business_df["date"].dt.year == year]
+            useful_count = year_df['useful'].max()
+            best_count = year_df['stars_review'].max()
+            worst_count = year_df['stars_review'].min()
+            useful_review = my_business_df[my_business_df['useful'] == useful_count].reset_index().drop_duplicates(subset=["useful"])
+            useful_review['date'] = year
+            best_review = my_business_df[my_business_df['stars_review'] == best_count].reset_index().drop_duplicates(subset=["stars_review"])
+            worst_review = my_business_df[my_business_df['stars_review'] == worst_count].reset_index().drop_duplicates(subset=["stars_review"])
+            reviews_cols['useful'][year] = useful_review['text'].item()
+            reviews_cols['best'][year] = best_review['text'].item()
+            reviews_cols['worst'][year] = worst_review['text'].item()
+
+        most_useful_df = pd.DataFrame.from_dict(reviews_cols['useful'].items())
+        most_useful_df.columns = ['Year', 'Voted most useful review']
+        most_useful_df.set_index('Year', inplace=True)
+        date_df = date_df.join(most_useful_df.astype(str))
+
+        best_df = pd.DataFrame.from_dict(reviews_cols['best'].items())
+        best_df.columns = ['Year', 'Most Positive Review']
+        best_df.set_index('Year', inplace=True)
+        date_df = date_df.join(best_df.astype(str))
+
+        worst_df = pd.DataFrame.from_dict(reviews_cols['worst'].items())
+        worst_df.columns = ['Year', 'Most Negative Review']
+        worst_df.set_index('Year', inplace=True)
+        date_df = date_df.join(worst_df.astype(str))
+
+        date_df = date_df.reset_index()
+
+        ratings_chart = alt.Chart(date_df).encode(
+            x=alt.X('date:T', title="Year"),
+            y=alt.Y('stars_review:Q', title = "Average Star rating from Reviews"),
+            tooltip = ['stars_review', 'Voted most useful review', 'Most Positive Review', 'Most Negative Review']
+        ).properties(
+            title="Average Star Ratings over Time",
+            width=800,
+            height=400
+        )
+        st.altair_chart((ratings_chart.mark_line() + ratings_chart.mark_point(filled=True, size=40)).interactive())
+        return feature_selectbox
 
 
 def display_graph(selection="Hello"):
@@ -222,12 +247,6 @@ def display_graph(selection="Hello"):
         obj.write_text()
         obj.overall_bar()
     elif selection == "Your Restaurant":
-        st.title("Analyse your own business!")
-        st.markdown('Hello Owner, we are displaying your reviews in the form of wordclouds from both the negative and positive reviews this gives you an idea as to your specific shortcomings and criticisms mentioned by the customers. It also gives you an idea as to what went well so you can continue with those practices. \
-            The wordcloud we believe is helpful in highlighting the specific reasonings as to why your restaurant received low/high ratings. \
-            Providing it time based granularity gives the you the option to specifically look at what went right and what went wrong during a particular time duration.\
-            We also provide the you with visualizations that enable you to understand what attributes you lack that your competitors with better reviews exhibit as well as what attributes they share with competitors that have worse reviews.')
-
         business_name = specific_restaurant()
     elif selection == "Similarity Check":
         if business_name == None:
@@ -255,6 +274,7 @@ selector = st.sidebar.selectbox(
 )
 
 #  ------------------- Top Bar details here ----------------------
+
 
 
 
